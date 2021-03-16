ba75c50a9ebaaf813ef4e590f06b66d25e9d3240
Krzem <krzem5.dev@gmail.com>
2021-03-16 18:20:08 +0100

    Push Update 03/16/2021, 18:19:51

--- src/sudoku_solver/sudoku.c
+++ src/sudoku_solver/sudoku.c
@@ -1,120 +1,103 @@
 #include <sudoku.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <intrin.h>
+#pragma intrinsic(__popcnt16)
+#pragma intrinsic(__movsq)
+#pragma intrinsic(_BitScanForward)
 
 
 
-uint8_t _BIT_CNT[]={0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,4,5,5,6,5,6,6,7,5,6,6,7,6,7,7,8,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,4,5,5,6,5,6,6,7,5,6,6,7,6,7,7,8,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,4,5,5,6,5,6,6,7,5,6,6,7,6,7,7,8,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,4,5,5,6,5,6,6,7,5,6,6,7,6,7,7,8,4,5,5,6,5,6,6,7,5,6,6,7,6,7,7,8,5,6,6,7,6,7,7,8,6,7,7,8,7,8,8,9};
+typedef struct __SOLVE_BOARD{
+	uint8_t b[81];
+	uint16_t dt[27];
+} solve_board_t;
 
 
 
-uint8_t* _solve(uint8_t* b,uint16_t* dt){
-	while (true){
-		uint8_t mssl=10;
-		uint8_t msi;
-		uint8_t msj;
-		uint8_t msk;
-		uint8_t msl;
-		uint16_t mss;
-		bool mv=false;
-		for (uint8_t i=0;i<81;i++){
-			if (*(b+i)==0){
-				uint8_t j=i/9;
-				uint8_t k=i%9+9;
-				uint8_t l=j/3*3+k/3+15;
-				uint16_t s=(*(dt+j))&(*(dt+k))&(*(dt+l));
-				if (!s){
-					return NULL;
-				}
-				else if (!(s&(s-1))){
-					uint16_t m=~s;
-					_l:
-					(*(b+i))++;
-					if (s>>=1){
-						goto _l;
-					}
-					(*(dt+j))&=m;
-					(*(dt+k))&=m;
-					(*(dt+l))&=m;
-					mv=true;
-				}
-				else{
-					if (_BIT_CNT[s]<mssl){
-						mssl=_BIT_CNT[s];
-						msi=i;
-						msj=j;
-						msk=k;
-						msl=l;
-						mss=s;
-					}
-				}
+uint8_t _solve(solve_board_t* b,uint8_t* o){
+	uint8_t mssl=10;
+	uint8_t msi;
+	uint8_t msj;
+	uint8_t msk;
+	uint8_t msl;
+	uint16_t mss;
+_nxt_move:
+	for (uint8_t i=0;i<81;i++){
+		if (!b->b[i]){
+			uint8_t j=i/9;
+			uint8_t k=i%9+9;
+			uint8_t l=j/3*3+k/3+15;
+			uint16_t s=(b->dt[j])&(b->dt[k])&(b->dt[l]);
+			if (!s){
+				return 0;
 			}
-		}
-		if (mv==false){
-			// mssl=10;
-			if (mssl==10){
-				return b;
+			unsigned short bc=__popcnt16(s);
+			if (bc==1){
+				unsigned long lg;
+				_BitScanForward(&lg,s);
+				b->b[i]=(uint8_t)lg+1;
+				s=~s;
+				b->dt[j]&=s;
+				b->dt[k]&=s;
+				b->dt[l]&=s;
+				mssl|=16;
 			}
-			uint8_t n=0;
-			uint8_t* nb=malloc(81*sizeof(uint8_t));
-			uint16_t* ndt=malloc(27*sizeof(uint16_t));
-			uint16_t m=1;
-			for (uint8_t i=0;i<9;i++){
-				if (mss&m){
-					n++;
-					for (uint8_t j=0;j<81;j++){
-						*(nb+j)=*(b+j);
-						if (j<27){
-							*(ndt+j)=*(dt+j);
-						}
-					}
-					*(nb+msi)=i+1;
-					(*(ndt+msj))&=~m;
-					(*(ndt+msk))&=~m;
-					(*(ndt+msl))&=~m;
-					uint8_t* nbr=_solve(nb,ndt);
-					if (nbr!=NULL){
-						free(ndt);
-						if (nbr!=nb){
-							free(nb);
-						}
-						return nbr;
-					}
-					if (n==mssl){
-						free(nb);
-						free(ndt);
-						return NULL;
-					}
-				}
-				m<<=1;
+			else if (bc<(mssl&15)){
+				mssl=bc|(mssl&16);
+				msi=i;
+				msj=j;
+				msk=k;
+				msl=l;
+				mss=s;
 			}
 		}
 	}
+	if (mssl&16){
+		mssl=10;
+		goto _nxt_move;
+	}
+	if (mssl==10){
+		__movsq((uint64_t*)o,(uint64_t*)b->b,10);
+		*(o+80)=b->b[80];
+		return 1;
+	}
+	solve_board_t nb;
+	uint16_t m=1;
+	uint8_t n=0;
+	for (uint8_t i=0;i<9;i++){
+		if (mss&m){
+			__movsq((uint64_t*)(&nb),(uint64_t*)b,17);
+			nb.b[msi]=i+1;
+			uint16_t mm=~m;
+			nb.dt[msj]&=mm;
+			nb.dt[msk]&=mm;
+			nb.dt[msl]&=mm;
+			if (_solve(&nb,o)){
+				return 1;
+			}
+			n++;
+			if (n==mssl){
+				return 0;
+			}
+		}
+		m<<=1;
+	}
+	__assume(0);
 }
 
 
 
 void solve_sudoku(uint8_t* b){
-	uint16_t* dt=malloc(27*sizeof(uint16_t));
-	for (uint8_t i=0;i<27;i++){
-		*(dt+i)=0x1ff;
-	}
+	solve_board_t sb;
+	__stosw((unsigned short*)sb.dt,0x1ff,27);
 	for (uint8_t i=0;i<81;i++){
-		if (*(b+i)!=0){
+		if (sb.b[i]=*(b+i)){
 			uint8_t j=i/9;
 			uint8_t k=i%9+9;
 			uint16_t m=~(1<<((uint16_t)*(b+i)-1));
-			(*(dt+j))&=m;
-			(*(dt+k))&=m;
-			(*(dt+j/3*3+k/3+15))&=m;
+			sb.dt[j]&=m;
+			sb.dt[k]&=m;
+			sb.dt[j/3*3+k/3+15]&=m;
 		}
 	}
-	uint8_t* sb=_solve(b,dt);
-	free(dt);
-	if (sb!=b){
-		for (uint8_t i=0;i<81;i++){
-			*(b+i)=*(sb+i);
-		}
-		free(sb);
-	}
+	_solve(&sb,b);
 }
