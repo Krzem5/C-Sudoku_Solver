1048f8f0138d53e4aee8fa2f1761826c85f2fa97
Krzem <krzem5.dev@gmail.com>
2021-04-05 21:49:06 +0200

    Push Update 04/05/2021, 21:49:00

--- src/sudoku_solver/sudoku.c
+++ src/sudoku_solver/sudoku.c
@@ -9,24 +9,20 @@
 
 
 
-#define Z81_MASK 0x200
-
-
-
 typedef struct __SOLVE_BOARD{
 	uint64_t z64;
-	uint16_t z80;
+	uint32_t z32;
 	uint16_t dt[27];
 } solve_board_t;
 
 
 
 uint8_t _solve(solve_board_t* b,uint8_t* o){
-	unsigned short f=10;
+	uint8_t f=10;
 	uint8_t nmi;
 _nxt_move:
 	uint64_t z64=b->z64;
-	uint32_t z32=b->z80|(((uint32_t)b->dt[0]&Z81_MASK)<<7);
+	uint32_t z32=b->z32;
 	while (z64||z32){
 		unsigned long i;
 		if (z64){
@@ -35,13 +31,13 @@ _nxt_move:
 		}
 		else{
 			_BitScanForward(&i,z32);
-			z32&=~(1u<<i);
+			z32&=~(1<<i);
 			i+=64;
 		}
 		uint16_t* j=b->dt+i/9;
 		uint16_t* k=b->dt+i%9+9;
 		uint16_t* l=b->dt+i/27*3+(i%9)/3+18;
-		uint16_t s=((*j)&0x1ff)&(*k)&(*l);
+		uint16_t s=(*j)&(*k)&(*l);
 		if (!s){
 			return 0;
 		}
@@ -53,11 +49,8 @@ _nxt_move:
 			if (i<64){
 				b->z64&=~(1ull<<i);
 			}
-			else if (i<80){
-				b->z80&=~(1u<<(i-64));
-			}
 			else{
-				b->dt[0]&=~Z81_MASK;
+				b->z32&=~(1<<(i-64));
 			}
 			(*j)&=~s;
 			(*k)&=~s;
@@ -65,7 +58,7 @@ _nxt_move:
 			f=0;
 		}
 		else if (f&&bc<f){
-			f=bc;
+			f=(uint8_t)bc;
 			nmi=(uint8_t)i;
 		}
 	}
@@ -79,16 +72,13 @@ _nxt_move:
 	if (nmi<64){
 		b->z64&=~(1ull<<nmi);
 	}
-	else if (nmi<80){
-		b->z80&=~(1u<<(nmi-64));
-	}
 	else{
-		b->dt[0]&=~Z81_MASK;
+		b->z32&=~(1<<(nmi-64));
 	}
-	uint8_t j=(nmi)/9;
-	uint8_t k=(nmi)%9+9;
+	uint8_t j=nmi/9;
+	uint8_t k=nmi%9+9;
 	uint8_t l=j/3*3+k/3+15;
-	uint16_t s=(b->dt[j]&0x1ff)&(b->dt[k])&(b->dt[l]);
+	uint16_t s=(b->dt[j])&(b->dt[k])&(b->dt[l]);
 	solve_board_t nb;
 	uint16_t* nb_j=nb.dt+j;
 	uint16_t* nb_k=nb.dt+k;
@@ -97,6 +87,7 @@ _check_all:
 	unsigned long i;
 	_BitScanForward(&i,s);
 	__movsq((uint64_t*)(&nb),(uint64_t*)b,8);
+	nb.dt[26]=b->dt[26];
 	uint16_t m=~(1<<(uint16_t)i);
 	s&=m;
 	(*nb_j)&=m;
@@ -132,11 +123,8 @@ uint8_t solve_sudoku(uint8_t* b){
 			if (i<64){
 				sb.z64|=(1ull<<i);
 			}
-			else if (i<80){
-				sb.z80|=(1<<(i-64));
-			}
 			else{
-				sb.dt[0]|=Z81_MASK;
+				sb.z32|=(1<<(i-64));
 			}
 		}
 	}
