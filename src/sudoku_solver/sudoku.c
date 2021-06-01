e4d9407fa9b24d0ae480ac26b6ec100ea6ef2f80
Krzem <krzem5.dev@gmail.com>
2021-06-01 18:30:17 +0200

    Push Update 06/01/2021, 18:30:15

--- src/sudoku_solver/sudoku.c
+++ src/sudoku_solver/sudoku.c
@@ -1,15 +1,13 @@
 #include <sudoku.h>
 #include <intrin.h>
 #include <stdint.h>
-#pragma intrinsic(__movsq)
 #pragma intrinsic(__popcnt16)
-#pragma intrinsic(__stosw)
 #pragma intrinsic(_BitScanForward)
 #pragma intrinsic(_BitScanForward64)
 
 
 
-#define Z81_MASK 0x200
+#define Z81_MASK 0x8000
 
 
 
@@ -22,57 +20,57 @@ typedef struct __SOLVE_BOARD{
 
 
 uint8_t _solve(solve_board_t* b,uint8_t* o){
-	unsigned short f=10;
+	unsigned short f;
 	uint8_t nmi;
-_nxt_move:
-	uint64_t z64=b->z64;
-	uint32_t z32=b->z80|(((uint32_t)b->dt[0]&Z81_MASK)<<7);
-	while (z64||z32){
-		unsigned long i;
-		if (z64){
-			_BitScanForward64(&i,z64);
-			z64&=~(1ull<<i);
-		}
-		else{
-			_BitScanForward(&i,z32);
-			z32&=~(1u<<i);
-			i+=64;
-		}
-		uint16_t* j=b->dt+i/9;
-		uint16_t* k=b->dt+i%9+9;
-		uint16_t* l=b->dt+i/27*3+(i%9)/3+18;
-		uint16_t s=((*j)&0x1ff)&(*k)&(*l);
-		if (!s){
-			return 0;
-		}
-		unsigned short bc=__popcnt16(s);
-		if (bc==1){
-			unsigned long bi;
-			_BitScanForward(&bi,s);
-			*(o+i)=(uint8_t)bi+1;
-			if (i<64){
-				b->z64&=~(1ull<<i);
-			}
-			else if (i<80){
-				b->z80&=~(1u<<(i-64));
+	uint32_t z81=(b->dt[0]&Z81_MASK);
+	b->dt[0]&=~Z81_MASK;
+	do{
+		f=10;
+		uint64_t z64=b->z64;
+		uint32_t z32=b->z80|(z81<<1);
+		while (z64||z32){
+			unsigned long i;
+			if (z64){
+				_BitScanForward64(&i,z64);
+				z64&=~(1ull<<i);
 			}
 			else{
-				b->dt[0]&=~Z81_MASK;
+				_BitScanForward(&i,z32);
+				z32&=~(1u<<i);
+				i+=64;
 			}
-			(*j)&=~s;
-			(*k)&=~s;
-			(*l)&=~s;
-			f=0;
+			uint16_t* j=b->dt+i/9;
+			uint16_t* k=b->dt+i%9+9;
+			uint16_t* l=b->dt+i/27*3+(i%9)/3+18;
+			uint16_t s=(*j)&(*k)&(*l);
+			if (!s){
+				return 0;
+			}
+			unsigned short bc=__popcnt16(s);
+			if (bc==1){
+				unsigned long bi;
+				_BitScanForward(&bi,s);
+				*(o+i)=(uint8_t)bi+1;
+				if (i<64){
+					b->z64&=~(1ull<<i);
+				}
+				else if (i<80){
+					b->z80&=~(1u<<(i-64));
+				}
+				else{
+					z81=0;
+				}
+				(*j)&=~s;
+				(*k)&=~s;
+				(*l)&=~s;
+				f=0;
+			}
+			else if (f&&bc<f){
+				f=bc;
+				nmi=(uint8_t)i;
+			}
 		}
-		else if (f&&bc<f){
-			f=bc;
-			nmi=(uint8_t)i;
-		}
-	}
-	if (!f){
-		f=10;
-		goto _nxt_move;
-	}
+	} while (!f);
 	if (f==10){
 		return 1;
 	}
@@ -88,28 +86,29 @@ _nxt_move:
 	uint8_t j=(nmi)/9;
 	uint8_t k=(nmi)%9+9;
 	uint8_t l=j/3*3+k/3+15;
-	uint16_t s=(b->dt[j]&0x1ff)&(b->dt[k])&(b->dt[l]);
-	solve_board_t nb;
+	uint16_t s=(b->dt[j])&(b->dt[k])&(b->dt[l]);
+	b->dt[0]|=z81;
+	solve_board_t nb=*b;
 	uint16_t* nb_j=nb.dt+j;
 	uint16_t* nb_k=nb.dt+k;
 	uint16_t* nb_l=nb.dt+l;
-_check_all:
-	unsigned long i;
-	_BitScanForward(&i,s);
-	__movsq((uint64_t*)(&nb),(uint64_t*)b,8);
-	uint16_t m=~(1<<(uint16_t)i);
-	s&=m;
-	(*nb_j)&=m;
-	(*nb_k)&=m;
-	(*nb_l)&=m;
-	if (_solve(&nb,o)){
-		*(o+nmi)=(uint8_t)i+1;
-		return 1;
+	while (1){
+		unsigned long i;
+		_BitScanForward(&i,s);
+		uint16_t m=~(1<<(uint16_t)i);
+		s&=m;
+		(*nb_j)&=m;
+		(*nb_k)&=m;
+		(*nb_l)&=m;
+		if (_solve(&nb,o)){
+			*(o+nmi)=(uint8_t)i+1;
+			return 1;
+		}
+		if (!s){
+			return 0;
+		}
+		nb=*b;
 	}
-	if (s){
-		goto _check_all;
-	}
-	return 0;
 }
 
 
@@ -117,13 +116,40 @@ _check_all:
 uint8_t solve_sudoku(uint8_t* b){
 	solve_board_t sb={
 		0,
-		0
+		0,
+		{
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff,
+			0x1ff
+		}
 	};
-	__stosw((unsigned short*)sb.dt,0x1ff,27);
 	for (uint8_t i=0;i<81;i++){
-		uint8_t v=*(b+i);
-		if (v){
-			uint16_t m=~(1<<(v-1));
+		if (*(b+i)){
+			uint16_t m=~(1<<((*(b+i))-1));
 			sb.dt[i/9]&=m;
 			sb.dt[i%9+9]&=m;
 			sb.dt[i/27*3+(i%9)/3+18]&=m;
